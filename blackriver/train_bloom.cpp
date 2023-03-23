1576f760fdfcc3a9c0a403631555ae3e9385d114
Zhou Chang <achang.zhou@gmail.com>
2023-03-23 22:07:46 +0800

    clean code for mpi words

--- blackriver/train_bloom.cpp
+++ blackriver/train_bloom.cpp
@@ -22,6 +22,33 @@ inline std::string fileToString(const char* filename) {
     return str;
 }
 
+void init_env(br::Enviroment* env) {
+    std::string init_code = fileToString("model/init.words");
+    br::DaG* init_wd = env->build(init_code);
+
+    // 0th hash  is used in GPU
+    env->change(0);
+    env->hash().set("$DEVICE", "cuda");
+    env->run(init_wd);
+    env->execute("create_weight");
+    env->execute("create_grad");
+    env->execute("create_var");
+
+    // others is used in CPU
+    for (size_t i = 1; i < env->hashes_num(); i++) {
+        env->change(i);
+        env->hash().set("$DEVICE", "cpu");
+        env->run( init_wd );
+        env->execute("create_weight");
+        env->execute("create_grad");
+    }
+
+    env->change(1);
+    env->execute("'h0' load_weight");
+    env->execute("1 sync");
+    delete init_wd;
+}
+
 int main(int argc, char* argv[] ) {
     br::CollectiveContext::boot(argc, argv, 2);
 
@@ -30,43 +57,17 @@ int main(int argc, char* argv[] ) {
         br::load_data("model/xinput.msg", xinput);
 
         MPI_Send(xinput.data(), xinput.size(), MPI_FLOAT, 1, 0, MPI_COMM_WORLD);
-
     } else if ( br::CollectiveContext::mpi_rank == 1) {
         br::ComputingContext::boot( br::CollectiveContext::nccl_rank );
         br::Enviroment* env = new br::Enviroment(15 + 1);
         br::load_nn_words(*env);
 
-        // create weight/grad/var for every layer , zero layer is GPU
-        {
-            std::string init_code = fileToString("model/init.words");
-            br::DaG* init_wd = env->build(init_code);
-
-            env->change(0);
-            env->hash().set("$DEVICE", "cuda");
-            env->run(init_wd);
-            env->execute("create_weight");
-            env->execute("create_grad");
-            env->execute("create_var");
-
-            for (int i = 1; i <= 15; i++) {
-                env->change(i);
-                env->hash().set("$DEVICE", "cpu");
-                env->run( init_wd );
-                env->execute("create_weight");
-                env->execute("create_grad");
-            }
-
-            env->change(1);
-            env->execute("'h0' load_weight");
-            env->execute("1 sync");
-            delete init_wd;
-        }
+        init_env(env);
 
         env->change(0);
         br::tensor_t xinput = env->hash().find_tensor("xinput");
         br::tensor_t xinput_ = env->hash().find_tensor("xinput_");
         xinput_->io_mpi_recv(xinput_, 0);
-
         xinput->op_copy(xinput, xinput_);
 
         {
